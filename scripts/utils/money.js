befb9a21cbd4f1ea36a1f1e15f1ecb43982089ce
NeTut007 <168202664+NeTut007@users.noreply.github.com>
2025-11-06 00:51:46 +0300

    Save data and generate HTML for deliveryOptions

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,5 @@
 export function formatCurrency(priceCents) {
     return (priceCents / 100).toFixed(2);   
 }
+
+export default formatCurrency; // если написан так export то надо по другому писать import| import НО БЕЗ {}
